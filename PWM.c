1f77315238e1d54b31ac548bb901d6cdd2cffe67
Julian Torres <yogurtcrampz@gmail.com>
2018-06-05 16:27:20 -0700

    Added PWM files for use with PF1 and PF2

--- PWM.c
+++ PWM.c
@@ -1,127 +1,147 @@
 //****************************************************************************
-// PWM.c
-// Runs MSP432E401Y
-// Use M0PWM0/PF0 and M0PWM1/PF1 to generate pulse-width modulated outputs.
-//
-// Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-//    You may use, edit, run or distribute this file
-//    as long as the above copyright notice remains
-//
-// The system clock SYSCLK is assumed to be running at 120Mhz
-// Change PWM_CC_PWMDIV_2 to prescale SYSCLK enought to achieve
-// a 20millisecond PWM period using a 16 bit counter
+// PWM.c                                                                     *
+// Runs on MSP432E401Y                                                       *
+// Derived from J.W. Valvano's PWM.c file      L.Aamodt  5/27/18             *
+//    Converted to standard CMSIS naming.                                    * 
+// Initialization and duty cycle setting routines for the PWM0 module on the *
+// MSP432E401Y which has one PWM module with four generators, 2 outputs each *
+//   PWM generator 0 output A that drives port PF0                           *
+//   PWM generator 0 output B that drives port PF1                           *
+//   PWM generator 1 output A that drives port PF2                           *
+//                                                                           *
+// Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu            *
+//    You may use, edit, run or distribute this file                         *
+//    as long as the above copyright notice remains                          *
+//                                                                           *
+// The system clock (SYSCLK) is assumed to be running at 120Mhz              *
+//   period is a 16-bit number of PWM clock cycles in one period (3<=period) *
+//   duty is number of PWM clock cycles output is high  (2<=duty<=period-1)  *
+// PWM CC register is set to prescale SYSCLK by 64 to give 1.875Mhz PWM clock*
+//   Set period = 37500 to get 50Hz pulse period                             *
+//   Set duty = 2812 to get 1500us pulse width                               *
+// Note: Port PF0 on the MSP-EXP432E401Y board is connected to LED D4        *
 //****************************************************************************
 
 #include <stdint.h>
+#include "msp432e401y.h"
 
-#define PWM0_ENABLE_R           (*((volatile uint32_t *)0x40028008))
-#define PWM_ENABLE_PWM1EN       0x00000002  // MnPWM1 Output Enable
-#define PWM_ENABLE_PWM0EN       0x00000001  // MnPWM0 Output Enable
-#define PWM0_0_CTL_R            (*((volatile uint32_t *)0x40028040))
-#define PWM_0_CTL_ENABLE        0x00000001  // PWM Block Enable
-#define PWM0_0_LOAD_R           (*((volatile uint32_t *)0x40028050))
-#define PWM0_0_COUNT_R          (*((volatile uint32_t *)0x40028054))
-#define PWM0_0_CMPA_R           (*((volatile uint32_t *)0x40028058))
-#define PWM0_0_CMPB_R           (*((volatile uint32_t *)0x4002805C))
-#define PWM0_0_GENA_R           (*((volatile uint32_t *)0x40028060))
-#define PWM_0_GENA_ACTCMPAD_ONE 0x000000C0  // Set the output signal to 1
-#define PWM_0_GENA_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
-#define PWM0_0_GENB_R           (*((volatile uint32_t *)0x40028064))
-#define PWM_0_GENB_ACTCMPBD_ONE 0x00000C00  // Set the output signal to 1
-#define PWM_0_GENB_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
-#define PWM0_CC_R               (*((volatile uint32_t *)0x40028FC8))
-#define PWM_CC_USEPWM           0x00000100  // Use PWM Clock Divisor
-#define PWM_CC_PWMDIV_M         0x00000007  // PWM Clock Divider
-#define PWM_CC_PWMDIV_2         0x00000005  // /64 ///////////////// then divide by 0x927C
-#define GPIO_PORTF_AFSEL_R      (*((volatile uint32_t *)0x4005D420))
-#define GPIO_PORTF_DEN_R        (*((volatile uint32_t *)0x4005D51C))
-#define GPIO_PORTF_AMSEL_R      (*((volatile uint32_t *)0x4005D528))
-#define GPIO_PORTF_PCTL_R       (*((volatile uint32_t *)0x4005D52C))
-#define SYSCTL_RCGCGPIO_R       (*((volatile uint32_t *)0x400FE608))
-#define SYSCTL_RCGCGPIO_R5      0x00000020  // GPIO Port F Run Mode Clock
-                                            // Gating Control
-#define SYSCTL_RCGCPWM_R        (*((volatile uint32_t *)0x400FE640))
-#define SYSCTL_RCGCPWM_R0       0x00000001  // PWM Module 0 Run Mode Clock
-                                            // Gating Control
-#define SYSCTL_PRGPIO_R         (*((volatile uint32_t *)0x400FEA08))
-#define SYSCTL_PRGPIO_R5        0x00000020  // GPIO Port F Peripheral Ready
-#define SYSCTL_PRPWM_R          (*((volatile uint32_t *)0x400FEA40))
-#define SYSCTL_PRPWM_R0         0x00000001  // PWM Module 0 Peripheral Ready
 
-// period is 16-bit number of PWM clock cycles in one period (3<=period)
-// period for PF0 and PF1 must be the same
-// duty is number of PWM clock cycles output is high  (2<=duty<=period-1)
-// PWM clock rate = processor clock rate/SYSCTL_RCC_PWMDIV
-//                = BusClock/2
-//                = 120 MHz/2 = 60 MHz (in this example)
+//#define PWM_ENABLE_PWM1EN       0x00000002  // MnPWM1 Output Enable
+//#define PWM_ENABLE_PWM0EN       0x00000001  // MnPWM0 Output Enable
+//#define PWM_0_CTL_ENABLE        0x00000001  // PWM Block Enable
+//#define PWM_0_GENA_ACTCMPAD_ONE 0x000000C0  // Set the output signal to 1
+//#define PWM_0_GENA_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
+//#define PWM_0_GENB_ACTCMPBD_ONE 0x00000C00  // Set the output signal to 1
+//#define PWM_0_GENB_ACTLOAD_ZERO 0x00000008  // Set the output signal to 0
+//#define PWM_CC_USEPWM           0x00000100  // Use PWM Clock Divisor
+//#define PWM_CC_PWMDIV_M         0x00000007  // PWM Clock Divider
+//#define PWM_CC_PWMDIV_2         0x00000000  // /2
+//#define SYSCTL_RCGCGPIO_R5      0x00000020  // GPIO Port F Run Mode Clock
+//#define SYSCTL_RCGCPWM_R0       0x00000001  // PWM Module 0 Run Mode Clock
+//#define SYSCTL_PRGPIO_R5        0x00000020  // GPIO Port F Peripheral Ready
+//#define SYSCTL_PRPWM_R0         0x00000001  // PWM Module 0 Peripheral Ready
+
+//****************************************************************************
+
 // Output on PF0/M0PWM0
 void PWM0A_Init(uint16_t period, uint16_t duty){
-  SYSCTL_RCGCPWM_R |= SYSCTL_RCGCPWM_R0;// 1) activate clock for PWM0
+  SYSCTL->RCGCPWM |= SYSCTL_RCGCPWM_R0; // 1) activate clock for PWM0
                                         // 2) activate clock for Port F
-  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;
+  SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R5;
                                         // allow time for clock to stabilize
-  while((SYSCTL_PRGPIO_R&SYSCTL_PRGPIO_R5) == 0){};
-  GPIO_PORTF_AFSEL_R |= 0x01;           // 3) enable alt funct on PF0
-  GPIO_PORTF_DEN_R |= 0x01;             //    enable digital I/O on PF0
+  while((SYSCTL->PRGPIO&SYSCTL_PRGPIO_R5) == 0){};
+  GPIOF->AFSEL |= 0x01;                 // 3) enable alt funct on PF0
+  GPIOF->DEN |= 0x01;                   //    enable digital I/O on PF0
                                         // 4) configure PF0 as PWM0
-  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFFFF0)+0x00000006;
-  GPIO_PORTF_AMSEL_R &= ~0x01;          //    disable analog functionality on PF0
+  GPIOF->PCTL = (GPIOF->PCTL&0xFFFFFFF0)+0x00000006;
+  GPIOF->AMSEL &= ~0x01;          //    disable analog functionality on PF0
                                         // allow time for clock to stabilize
-  while((SYSCTL_PRPWM_R&SYSCTL_PRPWM_R0) == 0){};
-  PWM0_CC_R |= PWM_CC_USEPWM;           // 5) use PWM divider
-  PWM0_CC_R &= ~PWM_CC_PWMDIV_M;        //    clear PWM divider field
-  PWM0_CC_R += PWM_CC_PWMDIV_2;         //    configure for /2 divider
-  PWM0_0_CTL_R = 0;                     // 6) re-loading down-counting mode
-  PWM0_0_GENA_R = (PWM_0_GENA_ACTCMPAD_ONE|PWM_0_GENA_ACTLOAD_ZERO);
+  while((SYSCTL->PRPWM&SYSCTL_PRPWM_R0) == 0){};
+  PWM0->CC |= PWM_CC_USEPWM;            // 5) use PWM divider
+  PWM0->CC &= ~PWM_CC_PWMDIV_M;         //    clear PWM divider field
+  PWM0->CC += PWM_CC_PWMDIV_64;         //    configure for /64 divider
+  PWM0->_0_CTL = 0;                     // 6) re-loading down-counting mode
+  PWM0->_0_GENA = (PWM_0_GENA_ACTCMPAD_ONE|PWM_0_GENA_ACTLOAD_ZERO);
   // PF0 goes low on LOAD
   // PF0 goes high on CMPA down
-  PWM0_0_LOAD_R = period - 1;           // 7) cycles needed to count down to 0
-  PWM0_0_CMPA_R = duty - 1;             // 8) count value when output rises
-  PWM0_0_CTL_R |= PWM_0_CTL_ENABLE;     // 9) start PWM0
-  PWM0_ENABLE_R |= PWM_ENABLE_PWM0EN;   // 10) enable PWM0A/PF0 outputs
+  PWM0->_0_LOAD = period - 1;           // 7) cycles needed to count down to 0
+  PWM0->_0_CMPA = duty - 1;             // 8) count value when output rises
+  PWM0->_0_CTL |= PWM_0_CTL_ENABLE;     // 9) start PWM0
+  PWM0->ENABLE |= PWM_ENABLE_PWM0EN;   // 10) enable PWM0A/PF0 outputs
 }
 
 // change duty cycle of PWM0A/PF0
 // duty is number of PWM clock cycles output is high  (2<=duty<=period-1)
 void PWM0A_Duty(uint16_t duty){
-  PWM0_0_CMPA_R = duty - 1;             // 8) count value when output rises
+  PWM0->_0_CMPA = duty - 1;             // 8) count value when output rises
 }
+//***************************************************************************
 
-// period is 16-bit number of PWM clock cycles in one period (3<=period)
-// period for PF0 and PF1 must be the same
-// duty is number of PWM clock cycles output is high  (2<=duty<=period-1)
-// PWM clock rate = processor clock rate/SYSCTL_RCC_PWMDIV
-//                = BusClock/2
-//                = 120 MHz/2 = 60 MHz (in this example)
 // Output on PF1/M0PWM1
 void PWM0B_Init(uint16_t period, uint16_t duty){
-  SYSCTL_RCGCPWM_R |= SYSCTL_RCGCPWM_R0;// 1) activate clock for PWM0
+  SYSCTL->RCGCPWM |= SYSCTL_RCGCPWM_R0; // 1) activate clock for PWM0
                                         // 2) activate clock for Port F
-  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5;
+  SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R5;
                                         // allow time for clock to stabilize
-  while((SYSCTL_PRGPIO_R&SYSCTL_PRGPIO_R5) == 0){};
-  GPIO_PORTF_AFSEL_R |= 0x02;           // 3) enable alt funct on PF1
-  GPIO_PORTF_DEN_R |= 0x02;             //    enable digital I/O on PF1
+  while((SYSCTL->PRGPIO&SYSCTL_PRGPIO_R5) == 0){};
+  GPIOF->AFSEL |= 0x02;                 // 3) enable alt funct on PF1
+  GPIOF->DEN |= 0x02;                   //    enable digital I/O on PF1
                                         // 4) configure PF1 as PWM0
-  GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFFF0F)+0x00000060;
-  GPIO_PORTF_AMSEL_R &= ~0x02;          //    disable analog functionality on PF1
+  GPIOF->PCTL = (GPIOF->PCTL&0xFFFFFF0F)+0x00000060;
+  GPIOF->AMSEL &= ~0x02;          //    disable analog functionality on PF1
                                         // allow time for clock to stabilize
-  while((SYSCTL_PRPWM_R&SYSCTL_PRPWM_R0) == 0){};
-  PWM0_CC_R |= PWM_CC_USEPWM;           // 5) use PWM divider
-  PWM0_CC_R &= ~PWM_CC_PWMDIV_M;        //    clear PWM divider field
-  PWM0_CC_R += PWM_CC_PWMDIV_2;         //    configure for /2 divider
-  PWM0_0_CTL_R = 0;                     // 6) re-loading down-counting mode
-  PWM0_0_GENB_R = (PWM_0_GENB_ACTCMPBD_ONE|PWM_0_GENB_ACTLOAD_ZERO);
+  while((SYSCTL->PRPWM&SYSCTL_PRPWM_R0) == 0){};
+  PWM0->CC |= PWM_CC_USEPWM;            // 5) use PWM divider
+  PWM0->CC &= ~PWM_CC_PWMDIV_M;         //    clear PWM divider field
+  PWM0->CC += PWM_CC_PWMDIV_64;         //    configure for /2 divider
+  PWM0->_0_CTL = 0;                     // 6) re-loading down-counting mode
+  PWM0->_0_GENB = (PWM_0_GENB_ACTCMPBD_ONE|PWM_0_GENB_ACTLOAD_ZERO);
   // PF1 goes low on LOAD
   // PF1 goes high on CMPB down
-  PWM0_0_LOAD_R = period - 1;           // 7) cycles needed to count down to 0
-  PWM0_0_CMPB_R = duty - 1;             // 8) count value when output rises
-  PWM0_0_CTL_R |= PWM_0_CTL_ENABLE;     // 9) start PWM0
-  PWM0_ENABLE_R |= PWM_ENABLE_PWM1EN;   // 10) enable PWM0B/PF1 outputs
+  PWM0->_0_LOAD = period - 1;           // 7) cycles needed to count down to 0
+  PWM0->_0_CMPB = duty - 1;             // 8) count value when output rises
+  PWM0->_0_CTL |= PWM_0_CTL_ENABLE;     // 9) start PWM0
+  PWM0->ENABLE |= PWM_ENABLE_PWM1EN;    // 10) enable PWM0B/PF1 outputs
 }
 
 // change duty cycle of PWM0B/PF1
 // duty is number of PWM clock cycles output is high  (2<=duty<=period-1)
 void PWM0B_Duty(uint16_t duty){
-  PWM0_0_CMPB_R = duty - 1;             // 8) count value when output rises
+  PWM0->_0_CMPB = duty - 1;             // 8) count value when output rises
 }
+//****************************************************************************
+
+// Output on PF2/M0PWM2
+void PWM1A_Init(uint16_t period, uint16_t duty){
+  SYSCTL->RCGCPWM |= SYSCTL_RCGCPWM_R0; // 1) activate clock for PWM0
+                                        // 2) activate clock for Port F
+  SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R5;
+                                        // allow time for clock to stabilize
+  while((SYSCTL->PRGPIO&SYSCTL_PRGPIO_R5) == 0){};
+  GPIOF->AFSEL |= 0x04;                 // 3) enable alt funct on PF2
+  GPIOF->DEN |= 0x04;                   //    enable digital I/O on PF2
+                                        // 4) configure PF2 as PWM0
+  GPIOF->PCTL = (GPIOF->PCTL&0xFFFFF0FF)+0x00000600;
+  GPIOF->AMSEL &= ~0x04;          //    disable analog functionality on PF2
+                                        // allow time for clock to stabilize
+  while((SYSCTL->PRPWM&SYSCTL_PRPWM_R0) == 0){};
+  PWM0->CC |= PWM_CC_USEPWM;            // 5) use PWM divider
+  PWM0->CC &= ~PWM_CC_PWMDIV_M;         //    clear PWM divider field
+  PWM0->CC += PWM_CC_PWMDIV_64;         //    configure for /2 divider
+  PWM0->_1_CTL = 0;                     // 6) re-loading down-counting mode
+  PWM0->_1_GENA = (PWM_1_GENA_ACTCMPAD_ONE|PWM_1_GENA_ACTLOAD_ZERO);
+  // PF2 goes low on LOAD
+  // PF2 goes high on CMPA down
+  PWM0->_1_LOAD = period - 1;           // 7) cycles needed to count down to 0
+  PWM0->_1_CMPA = duty - 1;             // 8) count value when output rises
+  PWM0->_1_CTL |= PWM_1_CTL_ENABLE;     // 9) start PWM1
+  PWM0->ENABLE |= PWM_ENABLE_PWM2EN;   // 10) enable PWM1A/PF2 outputs
+} 
+
+// change duty cycle of PWM1A/PF2
+// duty is number of PWM clock cycles output is high  (2<=duty<=period-1)
+void PWM1A_Duty(uint16_t duty){
+  PWM0->_1_CMPA = duty - 1;             // 8) count value when output rises
+}
+
+
--- PWMTestMain.c
+++ PWMTestMain.c
@@ -1,117 +1,39 @@
-////****************************************************************************
-//// Name: Julian Torres
-//// File: rtos_user.c
-//// Class: CPTR 480
-//// HW: Lab 5
-//// Date: 5/22/2018
-////
-//// Requirements: Uses LCD screen connected to pin PC5. Pulse is transmitted through PF0.
-////
-//// Purpose / Results: The purpose of this lab was to test the PWM module for controlling servo motors.
-//// 										This program generates a PWM signal whose pulse width can be changed up and down
-////										with SW1 and SW2. The current pulse width is displayed on the LCD screen through 
-////										UART7 on PC5, with units of counts. The max value that the program allows is roughly 
-////										about 90% of the entire signal length (so it never reaches 100%).
-////
-//// Issues:		I didn't run into a whole lot of issues with this one, it just took some time reading the
-////						datasheet to figure out how the duty cycle is works with the functions given in PWM.c. I 
-////						initially forgot to debounce the switches so it took quite a bit of time to understand why
-////						my pulse width was going from 0% to about 90% with a single button press.
+//****************************************************************************
+// PWMTestMain.c                                                             *
+// Runs on MSP432E401Y                                                       *
+// Program for testing routines in PWM.c                                     *
+//   First call one of the Init functions.                                   *
+//   Call a Duty function to change the duty cycle                           *
+//                                                                           *
+// Use PWM0B/PF1 and PWM1A/PF2 to generate pulse-width modulated outputs.    *
+//                                                                           *
+//   period is a 16-bit number of PWM clock cycles in one period (3<=period) *
+//   period for PF0 and PF1 must be the same,PF2 can differ from PF0&PF1     *
+//   duty is number of PWM clock cycles output is high  (2<=duty<=period-1)  *
+//   PWM clock rate = processor clock rate/SYSCTL_CC_PWMDIV_64               *
+//                  = BusClock/64                                            *
+//                  = 120 MHz/64 = 1875000 Hz                                *
+// Created 5/28/18    L.Aamodt                                               *
+//****************************************************************************
+#include <stdint.h>
+#include "PLL.h"
 
-////****************************************************************************
-//// PWMTestMain.c
-//// Runs on MSP432E401Y
-//// Use PWM0A/PF0 and PWM0B/PF1 to generate pulse-width modulated outputs.
-////
-////  Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-////
-////****************************************************************************
-//#include "msp.h"
-//#include <stdint.h>
-//#include "PLL.h"
-//#include "PWM.h"
-//#include "UART.h"
 
-//#define SW1_MASK (0x1)
-//#define SW2_MASK (0x2)
+void PWM0A_Init(uint16_t period, uint16_t duty);
+void PWM0B_Init(uint16_t period, uint16_t duty);
+void PWM1A_Init(uint16_t period, uint16_t duty);
+void PWM0A_Duty(uint16_t duty);
+void PWM0B_Duty(uint16_t duty);
+void PWM1A_Duty(uint16_t duty);
 
-//void Init_Switches(void);
+int main(void){
+	uint32_t count;
+	
+  PLL_Init();                   // bus clock at 120 MHz
+  PWM1A_Init(37500, 2812);      // initialize PWM1A/PF2, 50Hz, 1500us pulse width
+  PWM0B_Init(37500, 20000);      // initialize PWM0B/PF1, 50Hz, 1500us pulse width
 
-//int main(void){
-//	uint32_t sw1;
-//	uint32_t sw2;
-//	uint32_t period = 37500;
-//	uint32_t doody = 2500;
-//	
-//    // Halt watchdog timer
-//	SYSCTL->RCGCWD &= ~(SYSCTL_RCGCWD_R1 | SYSCTL_RCGCWD_R0);
-
-//	// Enable GPION, GPIOJ, GPIOF peripheral /////////////////only J needed
-//	SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R8;
-
-//	// Check if the peripheral access is enabled.
-//	while(!(SYSCTL->PRGPIO & SYSCTL_PRGPIO_R8));
-
-//	Init_Switches();
-//  PLL_Init();                      // bus clock at 120 MHz
-//	UART_Init();											//Initialize UART for screen.
-//  PWM0A_Init(period, doody); //7.5%       // initialize PWM0A/PF0, 1000 Hz, 75% duty 			PWM0A_Init(60000, 45000);
-//  PWM0B_Init(period, doody); //7.5%       // initialize PWM0B/PF1, 1000 Hz, 25% duty
-//	
-//	
-//	while(1) {
-//		//Mask or strip bits of address
-//		sw1 = GPIOJ->DATA & SW1_MASK;
-//		sw2 = GPIOJ->DATA & SW2_MASK;
-//		
-//		//SW1 logic;
-//		if (!(sw1 == SW1_MASK)) {
-//			if (doody <= 500)
-//				doody = 500;
-//			else
-//				doody = doody - 500;
-//		
-//		}
-//		//SW2 logic;
-//		if (!(sw2 == SW2_MASK)) {
-//			if (doody >= 33500)
-//				doody = 33500;
-//			else
-//				doody = doody + 500;
-//		}
-//		
-//		for (uint32_t i = 0; i < 5000000; i++);
-//		
-//		PWM0B_Duty(doody);
-//		
-//		UART_OutCommand(0xFE);		//Clear Screen
-//		UART_OutCommand(0x01);
-//		
-//		UART_OutString("Pulse Length");
-//		UART_OutCommand(0xFE);
-//		UART_OutCommand(0x80 + 64);
-//		UART_OutUDec(doody);			//Output current duty in units of count
-//		UART_OutChar('/');
-//		UART_OutUDec(37500);
-//	}
-//}
-
-//void Init_Switches() {
-//	/* Enable the GPIO pin for Switches (PJ0 & PJ1). (SW1 & SW2)*/
-//	
-//	//Enable pull up resistors
-//	GPIOJ->PUR |= BIT0;
-//	GPIOJ->PUR |= BIT1;
-//	//Disable pull down resistors
-//	GPIOJ->PDR &= ~(BIT0);
-//	GPIOJ->PDR &= ~(BIT1);
-//	
-//	//Set the pin direction and mode
-//	GPIOJ->DIR &= ~(BIT0);
-//	GPIOJ->DIR &= ~(BIT1);
-//	GPIOJ->AMSEL &= ~(BIT0);
-//	GPIOJ->AMSEL &= ~(BIT1);
-//	//Set pin type
-//	GPIOJ->DEN |= BIT0;
-//	GPIOJ->DEN |= BIT1;
-//}
+  while(1){
+		count ++;                   // do nothing
+  }
+}
