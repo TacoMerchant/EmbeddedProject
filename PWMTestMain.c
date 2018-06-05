8fd779c8f4d6bc134f86b071a3753f2ceff41fef
Julian Torres <yogurtcrampz@gmail.com>
2018-06-05 15:35:23 -0700

    gitignore2

--- PWMTestMain.c
+++ PWMTestMain.c
@@ -1,117 +1,117 @@
-//****************************************************************************
-// Name: Julian Torres
-// File: rtos_user.c
-// Class: CPTR 480
-// HW: Lab 5
-// Date: 5/22/2018
-//
-// Requirements: Uses LCD screen connected to pin PC5. Pulse is transmitted through PF0.
-//
-// Purpose / Results: The purpose of this lab was to test the PWM module for controlling servo motors.
-// 										This program generates a PWM signal whose pulse width can be changed up and down
-//										with SW1 and SW2. The current pulse width is displayed on the LCD screen through 
-//										UART7 on PC5, with units of counts. The max value that the program allows is roughly 
-//										about 90% of the entire signal length (so it never reaches 100%).
-//
-// Issues:		I didn't run into a whole lot of issues with this one, it just took some time reading the
-//						datasheet to figure out how the duty cycle is works with the functions given in PWM.c. I 
-//						initially forgot to debounce the switches so it took quite a bit of time to understand why
-//						my pulse width was going from 0% to about 90% with a single button press.
+////****************************************************************************
+//// Name: Julian Torres
+//// File: rtos_user.c
+//// Class: CPTR 480
+//// HW: Lab 5
+//// Date: 5/22/2018
+////
+//// Requirements: Uses LCD screen connected to pin PC5. Pulse is transmitted through PF0.
+////
+//// Purpose / Results: The purpose of this lab was to test the PWM module for controlling servo motors.
+//// 										This program generates a PWM signal whose pulse width can be changed up and down
+////										with SW1 and SW2. The current pulse width is displayed on the LCD screen through 
+////										UART7 on PC5, with units of counts. The max value that the program allows is roughly 
+////										about 90% of the entire signal length (so it never reaches 100%).
+////
+//// Issues:		I didn't run into a whole lot of issues with this one, it just took some time reading the
+////						datasheet to figure out how the duty cycle is works with the functions given in PWM.c. I 
+////						initially forgot to debounce the switches so it took quite a bit of time to understand why
+////						my pulse width was going from 0% to about 90% with a single button press.
 
-//****************************************************************************
-// PWMTestMain.c
-// Runs on MSP432E401Y
-// Use PWM0A/PF0 and PWM0B/PF1 to generate pulse-width modulated outputs.
-//
-//  Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
-//
-//****************************************************************************
-#include "msp.h"
-#include <stdint.h>
-#include "PLL.h"
-#include "PWM.h"
-#include "UART.h"
+////****************************************************************************
+//// PWMTestMain.c
+//// Runs on MSP432E401Y
+//// Use PWM0A/PF0 and PWM0B/PF1 to generate pulse-width modulated outputs.
+////
+////  Copyright 2014 by Jonathan W. Valvano, valvano@mail.utexas.edu
+////
+////****************************************************************************
+//#include "msp.h"
+//#include <stdint.h>
+//#include "PLL.h"
+//#include "PWM.h"
+//#include "UART.h"
 
-#define SW1_MASK (0x1)
-#define SW2_MASK (0x2)
+//#define SW1_MASK (0x1)
+//#define SW2_MASK (0x2)
 
-void Init_Switches(void);
+//void Init_Switches(void);
 
-int main(void){
-	uint32_t sw1;
-	uint32_t sw2;
-	uint32_t period = 37500;
-	uint32_t doody = 2500;
-	
-    // Halt watchdog timer
-	SYSCTL->RCGCWD &= ~(SYSCTL_RCGCWD_R1 | SYSCTL_RCGCWD_R0);
+//int main(void){
+//	uint32_t sw1;
+//	uint32_t sw2;
+//	uint32_t period = 37500;
+//	uint32_t doody = 2500;
+//	
+//    // Halt watchdog timer
+//	SYSCTL->RCGCWD &= ~(SYSCTL_RCGCWD_R1 | SYSCTL_RCGCWD_R0);
 
-	// Enable GPION, GPIOJ, GPIOF peripheral /////////////////only J needed
-	SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R8;
+//	// Enable GPION, GPIOJ, GPIOF peripheral /////////////////only J needed
+//	SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R8;
 
-	// Check if the peripheral access is enabled.
-	while(!(SYSCTL->PRGPIO & SYSCTL_PRGPIO_R8));
+//	// Check if the peripheral access is enabled.
+//	while(!(SYSCTL->PRGPIO & SYSCTL_PRGPIO_R8));
 
-	Init_Switches();
-  PLL_Init();                      // bus clock at 120 MHz
-	UART_Init();											//Initialize UART for screen.
-  PWM0A_Init(period, doody); //7.5%       // initialize PWM0A/PF0, 1000 Hz, 75% duty 			PWM0A_Init(60000, 45000);
-  PWM0B_Init(period, doody); //7.5%       // initialize PWM0B/PF1, 1000 Hz, 25% duty
-	
-	
-	while(1) {
-		//Mask or strip bits of address
-		sw1 = GPIOJ->DATA & SW1_MASK;
-		sw2 = GPIOJ->DATA & SW2_MASK;
-		
-		//SW1 logic;
-		if (!(sw1 == SW1_MASK)) {
-			if (doody <= 500)
-				doody = 500;
-			else
-				doody = doody - 500;
-		
-		}
-		//SW2 logic;
-		if (!(sw2 == SW2_MASK)) {
-			if (doody >= 33500)
-				doody = 33500;
-			else
-				doody = doody + 500;
-		}
-		
-		for (uint32_t i = 0; i < 5000000; i++);
-		
-		PWM0B_Duty(doody);
-		
-		UART_OutCommand(0xFE);		//Clear Screen
-		UART_OutCommand(0x01);
-		
-		UART_OutString("Pulse Length");
-		UART_OutCommand(0xFE);
-		UART_OutCommand(0x80 + 64);
-		UART_OutUDec(doody);			//Output current duty in units of count
-		UART_OutChar('/');
-		UART_OutUDec(37500);
-	}
-}
+//	Init_Switches();
+//  PLL_Init();                      // bus clock at 120 MHz
+//	UART_Init();											//Initialize UART for screen.
+//  PWM0A_Init(period, doody); //7.5%       // initialize PWM0A/PF0, 1000 Hz, 75% duty 			PWM0A_Init(60000, 45000);
+//  PWM0B_Init(period, doody); //7.5%       // initialize PWM0B/PF1, 1000 Hz, 25% duty
+//	
+//	
+//	while(1) {
+//		//Mask or strip bits of address
+//		sw1 = GPIOJ->DATA & SW1_MASK;
+//		sw2 = GPIOJ->DATA & SW2_MASK;
+//		
+//		//SW1 logic;
+//		if (!(sw1 == SW1_MASK)) {
+//			if (doody <= 500)
+//				doody = 500;
+//			else
+//				doody = doody - 500;
+//		
+//		}
+//		//SW2 logic;
+//		if (!(sw2 == SW2_MASK)) {
+//			if (doody >= 33500)
+//				doody = 33500;
+//			else
+//				doody = doody + 500;
+//		}
+//		
+//		for (uint32_t i = 0; i < 5000000; i++);
+//		
+//		PWM0B_Duty(doody);
+//		
+//		UART_OutCommand(0xFE);		//Clear Screen
+//		UART_OutCommand(0x01);
+//		
+//		UART_OutString("Pulse Length");
+//		UART_OutCommand(0xFE);
+//		UART_OutCommand(0x80 + 64);
+//		UART_OutUDec(doody);			//Output current duty in units of count
+//		UART_OutChar('/');
+//		UART_OutUDec(37500);
+//	}
+//}
 
-void Init_Switches() {
-	/* Enable the GPIO pin for Switches (PJ0 & PJ1). (SW1 & SW2)*/
-	
-	//Enable pull up resistors
-	GPIOJ->PUR |= BIT0;
-	GPIOJ->PUR |= BIT1;
-	//Disable pull down resistors
-	GPIOJ->PDR &= ~(BIT0);
-	GPIOJ->PDR &= ~(BIT1);
-	
-	//Set the pin direction and mode
-	GPIOJ->DIR &= ~(BIT0);
-	GPIOJ->DIR &= ~(BIT1);
-	GPIOJ->AMSEL &= ~(BIT0);
-	GPIOJ->AMSEL &= ~(BIT1);
-	//Set pin type
-	GPIOJ->DEN |= BIT0;
-	GPIOJ->DEN |= BIT1;
-}
+//void Init_Switches() {
+//	/* Enable the GPIO pin for Switches (PJ0 & PJ1). (SW1 & SW2)*/
+//	
+//	//Enable pull up resistors
+//	GPIOJ->PUR |= BIT0;
+//	GPIOJ->PUR |= BIT1;
+//	//Disable pull down resistors
+//	GPIOJ->PDR &= ~(BIT0);
+//	GPIOJ->PDR &= ~(BIT1);
+//	
+//	//Set the pin direction and mode
+//	GPIOJ->DIR &= ~(BIT0);
+//	GPIOJ->DIR &= ~(BIT1);
+//	GPIOJ->AMSEL &= ~(BIT0);
+//	GPIOJ->AMSEL &= ~(BIT1);
+//	//Set pin type
+//	GPIOJ->DEN |= BIT0;
+//	GPIOJ->DEN |= BIT1;
+//}
--- rtos_user.c
+++ rtos_user.c
@@ -1,138 +1,138 @@
-/*******************************************
-*	Name: Julian Torres
-*	File Name: rtos_user.c
-*	Purpose: to test sample RTOS by adding 4th thread
-* CPTR 480
-* HW6
-* 4/27/2018
-*	Purpose / Results: 	The purpose of this assignment is to modify a simple RTOS by adding a fourth thread
-						that turns on the 4th LED. After adding the fourth thread, the priorities of the four
-						tasks need to be modified so that they run in the order 3,1,4,2. Each LED needs to turn
-						on for a second and then turn off for a second. The interrupts need to occur 16 times a 
-						second so the reload value is given a value of 1,000,000 because the frequency is 16MHz.
-						A for loop delay is added to each task to spin for about a second, so the for loop counts
-						to 200,000 because each iteration takes about 5us. Everything works correctly now and it makes
-						me happy. 
+///*******************************************
+//*	Name: Julian Torres
+//*	File Name: rtos_user.c
+//*	Purpose: to test sample RTOS by adding 4th thread
+//* CPTR 480
+//* HW6
+//* 4/27/2018
+//*	Purpose / Results: 	The purpose of this assignment is to modify a simple RTOS by adding a fourth thread
+//						that turns on the 4th LED. After adding the fourth thread, the priorities of the four
+//						tasks need to be modified so that they run in the order 3,1,4,2. Each LED needs to turn
+//						on for a second and then turn off for a second. The interrupts need to occur 16 times a 
+//						second so the reload value is given a value of 1,000,000 because the frequency is 16MHz.
+//						A for loop delay is added to each task to spin for about a second, so the for loop counts
+//						to 200,000 because each iteration takes about 5us. Everything works correctly now and it makes
+//						me happy. 
 
-* Issues: 	I couldn't figure out why but trying to add in the fourth task initially made none of them work at all.
-						After giving up for awhile, I restarted from the beginning and got it to work. I found that the iteration 
-						of my for loop delay took about 5us rather than 10us, but that also has a variable increment instruction 
-						in it to keep the compiler from skipping the for loop.
-*******************************************/
-
-
-//****************************************************************************
-// rtos_user.c
-// Runs on a TI MSP-EXP432E401Y board
-// An example user program that initializes the simple operating system
-//   Schedules three independent threads using preemptive round robin  
-//   Each thread rapidly toggles an LED 
-//
-//  Based on a program written by:
-// Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-//    You may use, edit, run or distribute this file
-//    as long as the above copyright notice remains
-// Modified to use CMSIS definitions for registers
-// Uses the default internal system clock, 16Mhz, no PLL,
-//    as input to the SysTick 24 bit counter.
-//
-// Modified by L.Aamodt  4/25/18  Walla Walla University
-//****************************************************************************
-
-#include <msp.h>
-
-void OS_Init(void);
-int OS_AddThreads(void(*task0)(void),
-	                void(*task1)(void),
-                  void(*task2)(void),
-									void(*task3)(void)); ///////////
-void OS_Launch(uint32_t theTimeSlice);
+//* Issues: 	I couldn't figure out why but trying to add in the fourth task initially made none of them work at all.
+//						After giving up for awhile, I restarted from the beginning and got it to work. I found that the iteration 
+//						of my for loop delay took about 5us rather than 10us, but that also has a variable increment instruction 
+//						in it to keep the compiler from skipping the for loop.
+//*******************************************/
 
 
-#define SysCountReloadValue  0xF4240    // thread switch time in system time units
+////****************************************************************************
+//// rtos_user.c
+//// Runs on a TI MSP-EXP432E401Y board
+//// An example user program that initializes the simple operating system
+////   Schedules three independent threads using preemptive round robin  
+////   Each thread rapidly toggles an LED 
+////
+////  Based on a program written by:
+//// Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+////    You may use, edit, run or distribute this file
+////    as long as the above copyright notice remains
+//// Modified to use CMSIS definitions for registers
+//// Uses the default internal system clock, 16Mhz, no PLL,
+////    as input to the SysTick 24 bit counter.
+////
+//// Modified by L.Aamodt  4/25/18  Walla Walla University
+////****************************************************************************
 
-uint32_t Count1;   // number of times thread1 loops
-uint32_t Count2;   // number of times thread2 loops
-uint32_t Count3;   // number of times thread3 loops
-uint32_t Count4;   ///////////////////////////////
-uint32_t i1;       // inner loop count, thread 1
+//#include <msp.h>
 
-void GPIO_init() {
-	// Enable clock to PORTN and PORTF
-	SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R12 | SYSCTL_RCGCGPIO_R5;
-	while(!(SYSCTL->PRGPIO & SYSCTL_PRGPIO_R12));	
+//void OS_Init(void);
+//int OS_AddThreads(void(*task0)(void),
+//	                void(*task1)(void),
+//                  void(*task2)(void),
+//									void(*task3)(void)); ///////////
+//void OS_Launch(uint32_t theTimeSlice);
 
-	// Set drive strength
-	GPION->DR2R |= BIT1;	// D1
-	GPION->DR2R |= BIT0;	// D2
-	GPIOF->DR2R |= BIT4;	// D3
-	GPIOF->DR2R |= BIT0;  ///////////////D4
-	
-	// Enable digital I/O mode
-	GPION->DEN |= BIT1;
-	GPION->DEN |= BIT0;
-	GPIOF->DEN |= BIT4;
-	GPIOF->DEN |= BIT0; ////////////////
 
-	// Disable analog mode
-	GPION->AMSEL &= ~(BIT1);
-	GPION->AMSEL &= ~(BIT0);
-	GPIOF->AMSEL &= ~(BIT4);
-	GPIOF->AMSEL &= ~(BIT0); /////////////////
+//#define SysCountReloadValue  0xF4240    // thread switch time in system time units
 
-	// Set the pin direction as output
-	GPION->DIR |= BIT1;
-	GPION->DIR |= BIT0;
-	GPIOF->DIR |= BIT4;
-	GPIOF->DIR |= BIT0; ///////////////////
+//uint32_t Count1;   // number of times thread1 loops
+//uint32_t Count2;   // number of times thread2 loops
+//uint32_t Count3;   // number of times thread3 loops
+//uint32_t Count4;   ///////////////////////////////
+//uint32_t i1;       // inner loop count, thread 1
 
-	GPION->AFSEL = 0x00;
-	GPIOF->AFSEL = 0x00;
-}
+//void GPIO_init() {
+//	// Enable clock to PORTN and PORTF
+//	SYSCTL->RCGCGPIO |= SYSCTL_RCGCGPIO_R12 | SYSCTL_RCGCGPIO_R5;
+//	while(!(SYSCTL->PRGPIO & SYSCTL_PRGPIO_R12));	
 
-void Task1(void){
-  Count1 = 0;
-	
-  for(;;){
-		for (uint32_t i = 0; i < 200000; i++) {
-			Count1++;
-		}
-		GPION->DATA ^= BIT1;				// toggle D1
-  }
-}
-void Task2(void){
-  Count2 = 0;
-  for(;;){
-		for (uint32_t i = 0; i < 200000; i++) {
-			Count2++;
-		}
-		GPION->DATA ^= BIT0;				// toggle D2
-  }
-}
-void Task3(void){
-  Count3 = 0;
-  for(;;){
-		for (uint32_t i = 0; i < 200000; i++) {
-			Count3++;
-		}
-		GPIOF->DATA ^= BIT4;				// toggle D3
-  }
-}
+//	// Set drive strength
+//	GPION->DR2R |= BIT1;	// D1
+//	GPION->DR2R |= BIT0;	// D2
+//	GPIOF->DR2R |= BIT4;	// D3
+//	GPIOF->DR2R |= BIT0;  ///////////////D4
+//	
+//	// Enable digital I/O mode
+//	GPION->DEN |= BIT1;
+//	GPION->DEN |= BIT0;
+//	GPIOF->DEN |= BIT4;
+//	GPIOF->DEN |= BIT0; ////////////////
 
-void Task4(void){ ///////////////////////////////
-  Count4 = 0;
-  for(;;){
-    for (uint32_t i = 0; i < 200000; i++) {
-			Count4++;
-		}
-		GPIOF->DATA ^= BIT0;				// toggle D4
-  }
-}
+//	// Disable analog mode
+//	GPION->AMSEL &= ~(BIT1);
+//	GPION->AMSEL &= ~(BIT0);
+//	GPIOF->AMSEL &= ~(BIT4);
+//	GPIOF->AMSEL &= ~(BIT0); /////////////////
 
-int main(void){
-  OS_Init();           // initialize, disable interrupts
-	GPIO_init();					// initialize GPIO ports for 3 LEDs
-  OS_AddThreads(&Task3, &Task1, &Task4, &Task2);
-  OS_Launch(SysCountReloadValue); // doesn't return, interrupts enabled in here
-  return 0;             // this never executes
-}
+//	// Set the pin direction as output
+//	GPION->DIR |= BIT1;
+//	GPION->DIR |= BIT0;
+//	GPIOF->DIR |= BIT4;
+//	GPIOF->DIR |= BIT0; ///////////////////
+
+//	GPION->AFSEL = 0x00;
+//	GPIOF->AFSEL = 0x00;
+//}
+
+//void Task1(void){
+//  Count1 = 0;
+//	
+//  for(;;){
+//		for (uint32_t i = 0; i < 200000; i++) {
+//			Count1++;
+//		}
+//		GPION->DATA ^= BIT1;				// toggle D1
+//  }
+//}
+//void Task2(void){
+//  Count2 = 0;
+//  for(;;){
+//		for (uint32_t i = 0; i < 200000; i++) {
+//			Count2++;
+//		}
+//		GPION->DATA ^= BIT0;				// toggle D2
+//  }
+//}
+//void Task3(void){
+//  Count3 = 0;
+//  for(;;){
+//		for (uint32_t i = 0; i < 200000; i++) {
+//			Count3++;
+//		}
+//		GPIOF->DATA ^= BIT4;				// toggle D3
+//  }
+//}
+
+//void Task4(void){ ///////////////////////////////
+//  Count4 = 0;
+//  for(;;){
+//    for (uint32_t i = 0; i < 200000; i++) {
+//			Count4++;
+//		}
+//		GPIOF->DATA ^= BIT0;				// toggle D4
+//  }
+//}
+
+//int main(void){
+//  OS_Init();           // initialize, disable interrupts
+//	GPIO_init();					// initialize GPIO ports for 3 LEDs
+//  OS_AddThreads(&Task3, &Task1, &Task4, &Task2);
+//  OS_Launch(SysCountReloadValue); // doesn't return, interrupts enabled in here
+//  return 0;             // this never executes
+//}
